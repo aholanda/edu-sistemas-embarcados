a70664b77a5900ace3d5130b3d39d6699dfed652
Adriano Holanda <aholanda@usp.br>
2021-03-25 16:31:40 -0300

    fix minor error during RAM assignment

--- microcontrolador/8051/SEII-push-pop.asm
+++ microcontrolador/8051/SEII-push-pop.asm
@@ -1,7 +1,7 @@
 ; valores iniciais na RAM
 ; @10 <- 41
 ; @25 <- 50
-; @30 < 99
+; @30 <- 99
 PUSH 10H ; incrementa SP para 0x08 e escreve o valor contido em 10H em SP
 PUSH 25H ; incrementa SP para 0x09 e escreve o valor contido em 25H em SP
 PUSH 30H ; incrementa SP para 0x0A e escreve o valor contido em 30H em SP
