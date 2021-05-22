f7ff6dfa8ea7d6ec152fc30e49184d313a16fc8c
Adriano Holanda <aholanda@usp.br>
2021-05-22 17:41:22 -0300

    fix tabs

--- microcontrolador/8051/SEII-motor.asm
+++ microcontrolador/8051/SEII-motor.asm
@@ -8,22 +8,22 @@
 ;; repetindo este ciclo indefinidamente.
 start:
 	; sentido horário
-	SETB P3.0	; |  liga 			P3.0
-	CLR 	P3.1 ; |_ desliga P3.1
+	SETB P3.0	; |  liga P3.0
+	CLR  P3.1 ; |_ desliga P3.1
 	CALL delay
 	; sentido anti-horário
-	CLR 	P3.0		; |  desliga P3.0
-	SETB P3.1 	; |_ liga 								P3.1
+	CLR P3.0	; |  desliga 	P3.0
+	SETB P3.1 	; |_ liga 	P3.1
 	CALL delay
 	; para motor
-	CLR 	P3.0		; |  desliga P3.0
-	CLR	P3.1 	; |_ desliga P3.1
+	CLR P3.0	; |  desliga P3.0
+	CLR P3.1 	; |_ desliga P3.1
 	CALL delay
 	JMP 	start
 
 delay:
-	MOV 	A,#16
+	MOV A,#16
 loop:
 	SUBB A,#1
-	JNZ 	loop
+	JNZ  loop
 	RET
